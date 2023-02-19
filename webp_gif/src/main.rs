c33c2ce1206e9f4d5ad1fd6fbee5e4405b05f800
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2023-02-19 14:41:51 +0000

    🤣::🐎

--- webp_gif/src/main.rs
+++ webp_gif/src/main.rs
@@ -1,16 +1,21 @@
 /*
 Image conversion with https://lib.rs/crates/image
-- need to have webp-encoder installed - brew install webp 
+- need to have webp-encoder installed - brew install webp
 */
 
-//using the crate 
-
-use::image::*;
+use image::*;
 
 fn main() {
-    println!("Hello, world!");
-}
+    // Use the open function to load an image from a Path.
+    // `open` returns a `DynamicImage` on success.
+    let img = image::open("px/g.webp").unwrap();
 
-fn wp_conv() {
-    // Main function with the actual encoder 
+    // The dimensions method returns the images width and height.
+    println!("dimensions {:?}", img.dimensions());
+
+    // The color method returns the image's `ColorType`.
+    println!("{:?}", img.color());
+
+    // Write the contents of this image to the Writer in PNG format.
+    img.save("test.gif").unwrap();
 }
