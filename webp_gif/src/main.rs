f9e27e95b71677f99b896d592dbb7f3a2efb6926
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2023-02-19 15:13:27 +0000

    🤣::🐎

--- webp_gif/src/main.rs
+++ webp_gif/src/main.rs
@@ -3,19 +3,14 @@ Image conversion with https://lib.rs/crates/image
 - need to have webp-encoder installed - brew install webp
 */
 
-use image::*;
+use ::image::*;
+use std::io::*;
 
 fn main() {
-    // Use the open function to load an image from a Path.
-    // `open` returns a `DynamicImage` on success.
-    let img = image::open("px/g.webp").unwrap();
+    println!("ga");
+    webp_conv();
+}
 
-    // The dimensions method returns the images width and height.
-    println!("dimensions {:?}", img.dimensions());
-
-    // The color method returns the image's `ColorType`.
-    println!("{:?}", img.color());
-
-    // Write the contents of this image to the Writer in PNG format.
-    img.save("test.gif").unwrap();
+fn webp_conv() {
+    
 }
